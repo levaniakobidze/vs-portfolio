6c0efbef1503bd37babf56c9ae06fbed5afbae40
levaniakobidze <levaniakobidze25@gmail.com>
2024-02-18 18:03:44 +0400

    change cv section

--- src/components/AboutComponents/Cv/Cv.jsx
+++ src/components/AboutComponents/Cv/Cv.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import classes from "./Cv.module.css";
 import CvImg from "../../../assets/cv.png";
 import { AiOutlineDownload } from "react-icons/ai";
+import { FaGoogleDrive } from "react-icons/fa";
 
 function Cv() {
   return (
@@ -9,8 +10,22 @@ function Cv() {
       <img src={CvImg} alt="cv" />
       <div className={classes.download_btn_cont}>
         <button>
-          <span>Download</span>
+          <a href="../../../assets/cv.pdf" download>
+            Download
+          </a>
+
           <AiOutlineDownload className={classes.download_icon} />
+        </button>
+        <button>
+          <a
+            href="https://drive.google.com/file/d/1yYqeq7aqJMp2AA8PM4nU8skist9NpKcI/view?usp=sharing"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Google Drive
+          </a>
+
+          <FaGoogleDrive className={classes.download_icon} />
         </button>
       </div>
     </div>
